b78005018d488ec546237be77a1987da804ac58a
lazlostrange <74869041+lazlostrange@users.noreply.github.com>
2023-05-15 11:13:04 +0200

    added max number of tokens

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -3,17 +3,21 @@ import tiktoken
 # gets tokens for a string
 # input: string
 # output: list with tokens for string
-def tokens_from_string(string: str, encoding_name: str) -> list[int]:
+def tokens_from_string(string: str, encoding_name: str, max_token = 5000) -> list[int]:
     encoding = tiktoken.get_encoding(encoding_name)
-    return encoding.encode(string)
+    token = encoding.encode(string)
+    if len(token) > max_token:  # return None if number of tokens exceed maximal number of tokens
+    	#print("WARNING: Number of tokens for string \""+string+"\" exceed the maximal number of tokens:", max_token, "!")
+    	return [None]
+    return token
 
 # gets tokens for a list of strings
 # input: list of strings "chunks"
 # output: list of list with tokens for strings
-def tokens_for_chunks(chunks: list[str], encoding_name = "cl100k_base") -> list[list[int]]:
+def tokens_for_chunks(chunks: list[str], encoding_name = "cl100k_base", max_token = 5000) -> list[list[int]]:
     chunk_tokens = []
     for chunk in chunks:
-        tokens = tokens_from_string(chunk, encoding_name)
+        tokens = tokens_from_string(chunk, encoding_name, max_token)
         chunk_tokens.append(tokens)
     return chunk_tokens
 
