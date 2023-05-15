4d12cfd3ef9b6d8f0688ccf54f31d7a13f4a4d84
lazlostrange <74869041+lazlostrange@users.noreply.github.com>
2023-05-15 11:43:43 +0200

    added parameters force_cut and verbose

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -3,35 +3,36 @@ import tiktoken
 # gets tokens for a string
 # input: string
 # output: list with tokens for string
-def tokens_from_string(string: str, encoding_name: str, max_token = 5000) -> list[int]:
+def tokens_from_string(string: str, encoding_name = "cl100k_base", max_token = 8000, force_cut = False, verbose = False) -> list[int]:
     encoding = tiktoken.get_encoding(encoding_name)
     token = encoding.encode(string)
-    if len(token) > max_token:  # return None if number of tokens exceed maximal number of tokens
-    	#print("WARNING: Number of tokens for string \""+string+"\" exceed the maximal number of tokens:", max_token, "!")
-    	return [None]
+    if len(token) > max_token: 
+        if verbose:  # print warning if max tokens is exceeded
+    	    print("WARNING: Number of tokens for string \""+string+"\" exceed the maximal number of tokens:", max_token, "!")
+        if force_cut:  # cut off tokens after max number of tokens
+    		return token[:max_token]
+    	return [None]  # return None if number of tokens exceed maximal number of tokens
     return token
 
 # gets tokens for a list of strings
 # input: list of strings "chunks"
 # output: list of list with tokens for strings
-def tokens_for_chunks(chunks: list[str], encoding_name = "cl100k_base", max_token = 5000) -> list[list[int]]:
+def tokens_for_chunks(chunks: list[str], encoding_name = "cl100k_base", max_token = 8000, force_cut = False, verbose = False) -> list[list[int]]:
     chunk_tokens = []
     for chunk in chunks:
-        tokens = tokens_from_string(chunk, encoding_name, max_token)
+        tokens = tokens_from_string(chunk, encoding_name = encoding_name, max_token = max_token, force_cut = force_cut, verbose = verbose)
         chunk_tokens.append(tokens)
     return chunk_tokens
 
 def main():
     text = ["You are a big boy",
              "incomprehensibilities",
-             "ich", "Ich",
-             "er",
-             "ist",
-             ".", "?", ",",
-             "er ist ich."]
-    print("Text to encode:", text)
-    encoded_text = tokens_for_chunks(text)
-    print("Encoding:")
+             "Me"]
+    print("Text to encode:")
+    for t in text:
+    	print(t)
+    encoded_text = tokens_for_chunks(text, verbose = True)
+    print("Tokens for text:")
     for encoding in encoded_text:
         print(encoding)
     exit(0)
