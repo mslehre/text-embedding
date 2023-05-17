69a57999b60ae9ae50169ba285ca39f99f9dba47
lukasz <s-luresz@uni-greifswald.de>
2023-05-17 11:36:48 +0000

    Add two text lines to enter the files

--- website.py
+++ website.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-app = Flask(__main__)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hallo! Das ist die Hauptwebseite"
+    return '''<form>
+  <label for="fname">Enter first file file: </label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Enter second file:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>'''
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run()
