6a7290dce0107e7282b45e6c06d08cd2878b6cff
lukasz <s-luresz@uni-greifswald.de>
2023-05-17 18:43:15 +0000

    Added the bottom that collect the inpt data

--- website.py
+++ website.py
@@ -8,7 +8,9 @@ def home():
   <label for="fname">Enter first file file: </label><br>
   <input type="text" id="fname" name="fname"><br>
   <label for="lname">Enter second file:</label><br>
-  <input type="text" id="lname" name="lname">
+  <input type="text" id="lname" name="lname"><br><br>
+
+  <input type="submit">
 </form>'''
 
 if __name__ == '__main__':
