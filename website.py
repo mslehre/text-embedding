b99ad25634f5cb079f37576df614f7a0c10d97b4
lukasz <s-luresz@uni-greifswald.de>
2023-05-20 18:22:23 +0000

    Addad two text ares to show the content of loaded files

--- website.py
+++ website.py
@@ -1,5 +1,16 @@
 from flask import Flask
 
+file1text = open("test1.txt", "r")
+file2text = open("test2.txt", "r")
+
+testtext = "Das ist ein Testtext"
+
+t1=file1text.read()
+
+print(file1text.read())
+print(file2text.read())
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +21,16 @@ def home():
   <label for="lname">Enter second file:</label><br>
   <input type="text" id="lname" name="lname"><br><br>
 
-  <input type="submit">
+  <input type="submit"> <br><br>
+
+  <form>
+  <textarea name="file1" rows="10" cols="30">''' +t1+'''
+  </textarea>
+
+  <form>
+  <textarea name="file2" rows="10" cols="30">'''+testtext+'''
+  </textarea>
+
 </form>'''
 
 if __name__ == '__main__':
