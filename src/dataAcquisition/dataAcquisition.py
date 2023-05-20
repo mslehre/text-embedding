74d972bf4c9cf1046eb02038a216d8ac1d3000bf
MarGabor <mg164811@uni-greifwald.de>
2023-05-20 12:22:21 +0200

    headless browser now working, script execution not working yet

--- src/dataAcquisition/dataAcquisition.py
+++ src/dataAcquisition/dataAcquisition.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 import time
 
 
@@ -23,13 +25,22 @@ def getJsCode(path):
     print("hallo2")
     with open(path) as file:
         content = file.read()
+    print("Executing code: \n" + content)
     return content
 
-def exejscode(jscode, URL): #PhantomJS binary might be needed to be added to PATH
+def exejscode(jscode, URL): #geckodriver 0.33 added to PATH
     
-    driver = webdriver.PhantomJS()
+    options = Options()
+    options.add_argument("--headless=new")
+    serv = Service('/snap/bin/firefox.geckodriver') #driver path
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("network.cookie.cookieBehavior", 2) #option 2 (no cookies allowed) will make the page not load properly
+
+    driver = webdriver.Firefox(options=options, service=serv, firefox_profile=fp)
+    driver.set_window_size(800,600)
     driver.get(URL)
-    time.sleep(10)
+    print ("Headless Firefox Initialized")
+    time.sleep(30)
     result = driver.execute_script(jscode)
     driver.quit()
     return result
@@ -40,7 +51,7 @@ def main():
     #js code to be converted
     print("Hallo")
     jscode = getJsCode("linkgrabber.js")
-    URL = "https://www.google.com"
+    URL = "https://www.webofscience.com/wos/author/record/1177820"
     linklist = exejscode(jscode, URL)
     print(linklist)
 
---
+++ src/dataAcquisition/js_scripts/linkgrabber.js
@@ -0,0 +1,12 @@
+function grabLinks(){
+var x = document.querySelectorAll("a");
+var myarray = []
+for (var i=0; i<x.length; i++){
+var nametext = x[i].textContent;
+var cleantext = nametext.replace(/\s+/g, ' ').trim();
+var cleanlink = x[i].href;
+myarray.push([cleantext,cleanlink]);
+};
+return myarray
+}
+grabLinks()
