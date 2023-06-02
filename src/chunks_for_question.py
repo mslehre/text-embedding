9831b10ad191ab4961b0b871f6690ebde85ddf5c
Nora <nora@jungierek.de>
2023-06-02 13:41:22 +0200

    go through all chunks in a selected folder

--- src/chunks_for_question.py
+++ src/chunks_for_question.py
@@ -1,33 +1,53 @@
+import os
+
 import tokenizer
-#from compute_embedding import get_embedding
+from compute_embedding import get_embedding
 
 # What to think about:
 #   - how to get the chunks? -> from folder, directly?
-#   - get only tokens for each chunk or the text itself?
 #   - how to return the chunks: indices, directly, which format?
 #   - questiona as string or only tokens? both?
 
-def get_k_chunks_for_string(question: str, 
-                            k: int):
+def get_k_chunks_from_folder(question: str, 
+                             folder: str,
+                             k: int = 3) -> list[str]:
+    
     """Gets the k chunks that have the best cosine similarity with the
-    tokenized question.
+    tokenized question. The analysed chunks are taken from the named folder, 
+    every file in this folder is embedded and compared to the embedded 
+    question.
 
     Args:
         question (str): The string of the question to scompare the embeddings 
             of the chunks to.
+        folder (str): The relative path of the folder if one is currently in 
+            the data directory, since all data should be stored there.#
+            Example:    For the folder named "Example_chunks" in the data 
+                        directory the strings should be in the following 
+                        format: "Exampe_chunks" (without "/" at thebeginning)
+            If the folder is stored in another folder, then use the relative 
+            path without "/" at the beginning.
         k (int): Integer that indicates the number of chunks that are returned.
-
+            Default is set to 3 chunks for now.     
 
     Returns:
-        LIST? VECTOR? ONLY INDICES?
+        list[str]: The list that contains the names of the k files with the 
+            best cosine similiarity for the question orderd from best to least 
+            similar.
     """
-    pass
+    print(os.getcwd())
+    directory = os.getcwd() + "/../data/" + folder
+    for filename in os.listdir(directory):
+        print(filename)
 
 def main():
     """Main to test the function that gets the k best chunks for a question.
     """
     question = "What are the similarities of a banana and an apple?"
     tokens_quest = tokenizer.get_token_from_string(question)
+    a = get_k_chunks_from_folder(question="What color has an apple?",
+                                 folder="Example_chunks",
+                                 k=1)
     print(tokens_quest)
     exit(0)
 
