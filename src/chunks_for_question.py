30d68bd542db9c94a221f87d2eb6310800ae26ce
NoJungi <nora@jungierek.de>
2023-06-04 17:41:51 +0200

    finished to get best k filenames for a question

--- src/chunks_for_question.py
+++ src/chunks_for_question.py
@@ -1,7 +1,8 @@
 import os
 
-import tokenizer
-from compute_embedding import get_embedding
+import numpy as np
+from compute_embedding import embedding_from_string
+from openai.embeddings_utils import cosine_similarity
 
 # What to think about:
 #   - how to get the chunks? -> from folder, directly?
@@ -12,10 +13,10 @@ from compute_embedding import get_embedding
 
 def get_k_chunks_from_folder(question: str, 
                              folder: str,
-                             k: int = 3) -> list[str]:
+                             k: int = 1) -> list[str]:
     
     """Gets the k chunks that have the best cosine similarity with the
-    tokenized question. The analysed chunks are taken from the named folder, 
+    embedded question. The analysed chunks are taken from the named folder, 
     every file in this folder is embedded and compared to the embedded 
     question.
 
@@ -30,27 +31,77 @@ def get_k_chunks_from_folder(question: str,
             If the folder is stored in another folder, then use the relative 
             path without "/" at the beginning.
         k (int): Integer that indicates the number of chunks that are returned.
-            Default is set to 3 chunks for now.     
+            Default is set to 1 chunks for now.     
 
     Returns:
         list[str]: The list that contains the names of the k files with the 
-            best cosine similiarity for the question orderd from best to least 
+            best cosine similiarity for the question orderd from most to least 
             similar.
     """
+    # Embed the different chunks and compute cosine similarity with question:
     directory = os.getcwd() + "/../data/" + folder
-    for filename in os.listdir(directory):
+    file_list = os.listdir(directory)
+    string_list = []
+
+    for filename in file_list:
         # TODO: catch exceptions of wrong files/ folders...
-        print(filename)
+        with open(directory + "/" + filename,'r') as file:
+            data = file.read().replace('\n', ' ')  # linebreaks to whitespaces
+            string_list.append(data)
+    
+    chunks_embedding = []
+    for i in range(0,len(string_list)):
+        chunks_embedding.append(embedding_from_string(string=string_list[i]))
+    
+    # Get indices of best embeddings and return the filenames of the 
+    # corresponding files.
+    inds = get_embeddings_argsort(question=question,
+                                        embedding_list=chunks_embedding)
+    
+    return [file_list[i] for i in inds[0:k]]
+
+def get_embeddings_argsort(question: str, 
+                             embedding_list: list[float]) -> list[int]:
+    """Gets the argsort of the given embeddings from best cosine similarity to 
+    least similarity with the given question.
+
+    Args:
+        question (str): The string of the question to scompare the embeddings 
+            of the chunks to.
+        embedding_list (list[float]): The list containing the embeddings of the
+            chunks.   
+
+    Returns:
+        list[int]: The list that contains the indices of the argsort of the 
+            embeddings according to the cosine similiarity for the question 
+            orderd from most to least similar.
+    """
+    # Embed the question
+    quest_embedding = embedding_from_string(string=question)
+    similarities = []
+    for i in range(0,len(embedding_list)):
+        similarities.append(cosine_similarity(quest_embedding, 
+                                              embedding_list[i]))
+    
+    # Return the indices of the k best embeddings, the best results have the 
+    # biggest cosine similarity.
+    inds = np.array(similarities).argsort()[::-1]  
+    return inds
+
 
 def main():
     """Main to test the function that gets the k best chunks for a question.
     """
-    question = "What are the similarities of a banana and an apple?"
-    tokens_quest = tokenizer.get_token_from_string(question)
-    a = get_k_chunks_from_folder(question="What color has an apple?",
+    question = "What is a pizza?"
+    k = 1
+    a = get_k_chunks_from_folder(question=question,
                                  folder="Example_chunks",
-                                 k=1)
-    print(tokens_quest)
+                                 k=k)
+    if k == 1:
+        print(f'Question: {question} The best file is {a[0]}.')
+    else:
+        print(f'Question: {question} The list with the best {k} files is {a}.')
+
     exit(0)
 
 if __name__ == "__main__":
