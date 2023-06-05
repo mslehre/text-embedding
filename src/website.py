ddc1ea34f238de5d950369357d7ea71e74bae944
lukasz <s-luresz@uni-greifswald.de>
2023-06-05 14:09:10 +0000

    There are two input windows, which take the input of the text file. The concatenation the those inputs is shown in the third window after clicking the button

--- src/website.py
+++ src/website.py
@@ -6,68 +6,44 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''<form>
-  <label for="fname">Enter first file file: </label><br>
-  <input type="text" id="file1"><br>
-  <label for="lname">Enter second file:</label><br>
-  <input type="text" id="file2"><br><br>
+  <label for="fname">Enter first file text: </label><br>
+  <textarea name="file1" id="file1text" rows="10" cols="30"></textarea>
+  </form>
+  
+  <form>
+  <label for="lname">Enter second file text:</label><br>
+  <textarea name="file1" id="file2text" rows="10" cols="30"></textarea>
+  </form>
 
-  <button onclick="myFunction()"> Read in files </button> <br><br>
+
+  <button onclick="myFunction()"> Read in file texts </button> <br><br>
 
   <p id="demo"> </p>
   <p id="url"> </p>
 
   <script>
     function myFunction() {
-      let f1 = document.getElementById("file1").value;
-      let f2 = document.getElementById("file2").value;
+      let f1 = document.getElementById("file1text").value;
+      let f2 = document.getElementById("file2text").value;
       let actualUrl = window.location.href;
 
       document.getElementById("url").innerHTML="";
 
       if(!f1 || !f2){
         document.getElementById("demo").innerHTML="";
-        alert("The file name can not be empty!");
-
+        alert("The file text can not be empty!");
       }
       else {
-        window.open(actualUrl+"/file1/"+f1+"/file2/"+f2);
+        document.getElementById("file1a2").value=f1+f2;
         }
     }
   </script>
 
   <form>
-  <textarea name="file1" rows="10" cols="30">
-  </textarea>
-
-  <form>
-  <textarea name="file2" rows="10" cols="30">
+  <textarea id="file1a2" rows="10" cols="30">
   </textarea>
 
 </form>'''
-
-@app.route('/file1/<file1>/file2/<file2>')
-def input_files(file1, file2):
-
-  if not((path.exists(file1) and path.exists(file2))):
-    return "<p>File does not exist</p>"
-
-  file1input = open(file1, "r")
-  file2input = open(file2, "r")
-
-  file1text=file1input.read()
-  file2text=file2input.read()
-  
-  return '''
-  <label id="file1">file1: '''+file1+'''</label> <br>
-  <label id="file2">file2: '''+file2+'''</label> <br>
-  
-  <form>
-  <textarea id="file1content" name="file1" rows="10" cols="30">'''+file1text+'''</textarea>
-
-  <textarea id="file2content" name="file2" rows="10" cols="30">'''+file2text+'''</textarea>
-
-</form>
-'''
 
 if __name__ == '__main__':
     app.run()
