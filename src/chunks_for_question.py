6bca862c7d685ad860d76ac7116d50c340c6e50b
s-nojung <s-nojung@uni-greifswald.de>
2023-06-11 20:39:52 +0000

    uptaded comments

--- src/chunks_for_question.py
+++ src/chunks_for_question.py
@@ -14,24 +14,21 @@ def get_k_IDs(question: str,
               k: int = 5) -> list[int]:
     """Gets the IDs of the k chunks that have the best cosine similarity with 
     the embedded question. The embeddings of the chunks are given in the hdf5
-    file embeddin_file which should be locaded in the data directory of this
-    repository. The function gives back a list that contains the IDs of the k
-    best chunks starting with the best match for the question.
-    ATTENTION: At this point it only returns the indices, not IDs. It may be
-               added later.
+    file named after the string of 'embeddings_file' which should be locaded in
+    the data directory of this repository. The function gives back a list that 
+    contains the IDs of the k best chunks starting with the best match for the 
+    question.
 
     Args:
-        question (str): The string of the question to scompare the embeddings 
+        question (str): The string of the question to compare the embeddings 
             of the chunks to.
         embeddings_file (str): The relative path of the hdf5 file, that 
             contains the embeddings , if one is currently in the data 
             directory, since all data files should be stored there. 
-            Example:    For the file named "example_embeddings" in the data 
-                        directory the strings should be in the following 
-                        format: "example_embeddings" (without "/" at the 
-                        beginning)
-            If the file is stored in another folder, then use the relative 
-            path without "/" at the beginning. (e.g. "folder/file_name")
+            Example:    For the file named "example_embeddings" in the  
+                        directory "data/folder" the strings should be in the 
+                        following format: "foler/example_embeddings" 
+                        (without "/" at the beginning)
         k (int): Integer that indicates the number of chunks that are returned.   
 
     Returns:
@@ -42,11 +39,16 @@ def get_k_IDs(question: str,
     # Get the embeddings from the hpf5 file:
     file_path = os.getcwd() + "//../data/" + embeddings_file
     with h5py.File(file_path, 'r') as f_in:
-        file_keys = f_in.keys()
-        print(file_keys)
-        # get all embeddings, shoulb be list of lists:
-        embeddings = f_in['publication_embeddings'][:] 
-        id_list = f_in['author_ids'][:]
+        # get all embeddings and the id list:
+        for key in f_in.keys():
+            print(key)
+            if("embeddings" in key):
+                embeddings = f_in[key][:] 
+            elif("ids" in key):
+                id_list = f_in[key][:]
+            else:
+                print("Some keys of the hdf5 file could not be used.")
+
 
     # Compute IDs of the best embeddings and return the sorted list from 
     # biggest to smallest similarity:
@@ -58,57 +60,6 @@ def get_k_IDs(question: str,
     inds = [id_list[i] for i in inds] 
     # TODO: catch exception that k is bigger than number of chunks...
     return inds[0:k]
-
-    
-# For now leave the function here for test purposes, may delete later...:
-def get_k_chunks_from_folder(question: str = "What is a pizza?", 
-                             folder: str = "Example_chunks",
-                             k: int = 1) -> list[str]:
-    """Gets the k chunks that have the best cosine similarity with the
-    embedded question. The analysed chunks are taken from the named folder, 
-    every file in this folder is embedded and compared to the embedded 
-    question.
-
-    Args:
-        question (str): The string of the question to scompare the embeddings 
-            of the chunks to.
-        folder (str): The relative path of the folder if one is currently in 
-            the data directory, since all data should be stored there.#
-            Example:    For the folder named "Example_chunks" in the data 
-                        directory the strings should be in the following 
-                        format: "Exampe_chunks" (without "/" at thebeginning)
-            If the folder is stored in another folder, then use the relative 
-            path without "/" at the beginning.
-        k (int): Integer that indicates the number of chunks that are returned.
-            Default is set to 1 chunks for now.     
-
-    Returns:
-        list[str]: The list that contains the names of the k files with the 
-            best cosine similiarity for the question orderd from most to least 
-            similar.
-    """
-    # TODO: catch exceptions if k=0, No question, ...
-    # Embed the different chunks and compute cosine similarity with question:
-    directory = os.getcwd() + "/../data/" + folder
-    file_list = os.listdir(directory)
-    string_list = []
-
-    for filename in file_list:
-        # TODO: catch exceptions of wrong files/ folders...
-        with open(directory + "/" + filename,'r') as f_in:
-            data = f_in.read().replace('\n', ' ')  # linebreaks to whitespaces
-            string_list.append(data)
-    
-    chunks_embedding = []
-    for i in range(0,len(string_list)):
-        chunks_embedding.append(embedding_from_string(string=string_list[i]))
-    
-    # Get indices of best embeddings and return the filenames of the 
-    # corresponding files.
-    inds = get_embeddings_argsort(question=question,
-                                        embedding_list=chunks_embedding)
-    
-    return [file_list[i] for i in inds[0:k]]
 
 def get_embeddings_argsort(question: str, 
                            embedding_list: list[list[float]]) -> list[int]:
@@ -127,10 +78,10 @@ def get_embeddings_argsort(question: str,
             orderd from most to least similar.
     """
     # Embed the question
-    quest_embedding = embedding_from_string(string=question)
+    question_embedding = embedding_from_string(string=question)
     similarities = []
     for i in range(0,len(embedding_list)):
-        similarities.append(cosine_similarity(quest_embedding, 
+        similarities.append(cosine_similarity(question_embedding, 
                                               embedding_list[i]))
     
     # Return the indices of the k best embeddings, the best results have the 
