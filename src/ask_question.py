ab1feff17911af49a793aae8c181f4ee67107578
f-bayer15 <fb165560@uni-greifswald.de>
2023-06-16 12:48:33 +0000

    changed embedding in tokenizer call to fit the model, cleaned up comments

--- src/ask_question.py
+++ src/ask_question.py
@@ -1,56 +1,49 @@
-#example question: What are common interests for these two scientists, give documents: first list, second list....
 #set key with export OPENAI_API_KEY="..."
-
-#https://platform.openai.com/docs/guides/gpt/chat-completions-api
 
 import os
 import openai
 
-from build_prompt import prompt
+from build_prompt import get_prompt
 
+def get_answer(
+        query: str, 
+        text_dir: str,
+        index_list: list) -> str:
+    """From a question asked by the user, generate the answer
 
-def answer(
-  query: str, 
-  text_dir: str,
-  index_list: list
-  ) -> str:
-        """From a question asked by the user, generate the answer
+    Args:
+        query (str): Question asked by the user.
+        text_dir (str): Documents directory.
+        index_list (list): List of relevant docs.
 
-        Args:
-            query (str): Question asked by the user.
-            text_dir (str): Documents directory.
-            index_list (list): List of relevant docs.
+    Returns:
+        str: Answer generated with the LLM
+    """
 
-        Returns:
-            str: Answer generated with the LLM
-        """
-
-        #first read in docs given by list from the given directory
-        docs={}
-        for i in index_list:
-            this_chunk = open(text_dir + "/" + str(i) + ".txt", "r",encoding="UTF-8")
-            docs["chunk" + str(i)]=this_chunk.read()
-            this_chunk.close()
+    #first read in the chunks given by the index_list from the given directory
+    docs = {}
+    for i in index_list:
+        this_chunk = open(text_dir + "/" + str(i) + ".txt", "r",encoding="UTF-8")
+        docs["chunk" + str(i)] = this_chunk.read()
+        this_chunk.close()
         
-        #assemble the prompt
-        this_prompt=prompt(query, docs)
+    #assemble the prompt
+    this_prompt = get_prompt(query, docs)
 
-        #call openai to obtain a response
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=this_prompt,
-            temperature=0,
-            max_tokens=30,
-        )
+    #call openai to obtain a response
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=this_prompt,
+        temperature=0,
+        max_tokens=500,
+    )
 
-        result = response['choices'][0]['text']
-        return result
+    result = response['choices'][0]['text']
+    return result
     
 def test():
-        testq="Given these lists of favourite foods, what could these two people eat togethers?"
-        testdir="/home/fb165560/text-embedding/data/test"
-        testlist=[1,3]
-        testanswer=answer(query=testq,text_dir=testdir,index_list=testlist)
-        print(testanswer)
-
-test()
+    testq = "What are common research interests of these scientists?"
+    testdir = "data/example_pubs"
+    testlist = [2,4]
+    testanswer = get_answer(query=testq,text_dir=testdir,index_list=testlist)
+    print(testanswer)
--- src/build_prompt.py
+++ src/build_prompt.py
@@ -1,19 +1,29 @@
-def prompt(
-    query: str,
-    chunks: dict,
-) -> str:
+from tokenizer import get_token_from_string
+from tokenizer import get_string_from_tokens
 
-#key relevant or not? docs will probably all start with the name of the scientist so could leave key out
-#left to do: add upper limit for prompt length -> not too many tokens
-    context=""
+def get_prompt(
+    query: str,
+    chunks: dict,) -> str:
+    """From a question and relevant documents, build the prompt to ask the LLM.
+
+    Args:
+        query (str): Question to ask the LLM.
+        chunks (dict): Dictionary containing the text of the chunks as values.
+
+    Returns:
+        str: The completed prompt, combined from the template, query and chunks.
+    """
+
+    context = ""
     for key in chunks:
-        context+= key + ": " + chunks[key] + "\n"
+        context += chunks[key] + "\n"
 
     template=f"""You are a Bot assistant answering any questions about documents.
 You are given a question and a set of documents.
 If the user's question requires you to provide specific information from the documents, give your answer based only on the examples provided below. DON'T generate an answer that is NOT written in the provided examples.
 If you don't find the answer to the user's question with the examples provided to you below, answer that you didn't find the answer in the documentation and propose him to rephrase his query with more details.
 Use bullet points if you have to make a list, only if necessary.
+Finish by proposing your help for anything else.
 
 QUESTION: {query}
 
@@ -21,13 +31,15 @@ DOCUMENTS:
 =========
 {context}
 =========
-Finish by proposing your help for anything else.
 """
 
-    return template
+    #tokenize to make sure the prompt does not exceed the token limit, cut it off if it does
+    prompt_tokens = get_token_from_string(template,encoding_name="p50k_base", max_token=3500, force_cut=True, verbose=True)
+    prompt = get_string_from_tokens(prompt_tokens, encoding_name="p50k_base")
+    return prompt
 
-test_q="a test"
-test_chunks={'first': 'this is an apple', 'second': 'this is a banana', 'third': 'this is an orange'}
-
-testp=prompt(test_q, test_chunks)
-print(testp)
+def test():
+    test_q = "a test"
+    test_chunks = {'first': 'this is an apple', 'second': 'this is a banana', 'third': 'this is an orange'}
+    testp = get_prompt(test_q, test_chunks)
+    print(testp)
