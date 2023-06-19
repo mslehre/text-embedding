a09c2737484d12685719cb710358f87f041ffe02
lukasz <s-luresz@uni-greifswald.de>
2023-06-19 09:27:28 +0000

    Add filters criterion to the header page

--- readFromPdf.py
+++ readFromPdf.py
@@ -38,9 +38,11 @@ def pdfToTxt(pdfname):
     output = open(txtname, 'w')
     output.write(fileText)
 
+
+#An ganze txt-Datei anwenden
 def filterPageText(pageText):
     #header
-    x = re.search("^[nN]ichtamtliche Lesefassung.*", pageText)
+    x = re.search("^\s?[nN]ichtamtliche Lesefassung.*", pageText)
 
     if x:
         #remove the found text
@@ -48,21 +50,76 @@ def filterPageText(pageText):
 
     #TODO
     #filter the footer - common footer not found
-    #filter the page number
+    #filter the page number - remove the number at the beginning - dangerous
     #filter the table of contents?
     #title and date at the firt page
 
     #filter out the page numbers
     #still at work
-    y = re.search("^\s[0-9]", pageText)
+    #y = re.search("^\s[0-9]", pageText)
         
-    if y:
+    #if y:
         #print('Possible page number found')
         #print(y[0])
-        pageText = re.sub(y[0], '', pageText)
+        #pageText = re.sub(y[0], '', pageText)
+
+    #search title at the first page
+    z = re.search("[0-9]\sPrüfungs\s-\sund\sStudienordnung*", pageText)
+
+    if z:
+        pageText = re.sub(z[0], '', pageText)
+        pageText = removeEmptyLine(pageText)
+    
+
+    
+
+    #des Bachelorstudiengangs Biomathematik v
+    #an der Ernst -Moritz -Arndt -Universität Greifswald (v)
+    # Promotionsordnung 
+    #der Mathematisch -Naturwissenschaftlichen Fakultät  
+    #der Ernst -Moritz -Arndt -Universität Greifswald  v
+    #des Masterstudiengangs Mathematik v
+    # Vom 12. Februar 2018  (v) 
+    #
+
+
+    #Titel Bachelor line
+    bachelorLine = re.search("des\sBachelor*studiengangs*", pageText)
+    if bachelorLine:
+        pageText = re.sub(bachelorLine[0], '??', pageText)
+
+    masterLine = re.search("des\sMasterstudiengangs*", pageText)
+    if masterLine:
+        pageText = re.sub(masterLine[0], '', pageText)
+
+    uniNameHeader = re.search("an\sder\sErnst*Greifswald", pageText)
+    if uniNameHeader:
+        pageText = re.sub(uniNameHeader[0], '', pageText)
+
+    dateHeader = re.search("^Vom.20[0-9][0-9]", pageText)
+    if dateHeader:
+        pageText = re.sub(dateHeader[0], '', pageText)
+
+    promotionHeader = re.search("Promotionsordnung", pageText)
+    if promotionHeader:
+        pageText = re.sub(promotionHeader[0], '', pageText)
+
+    fakultaetHeader = re.search("der\sMathematisch.Fakultät", pageText)
+    if fakultaetHeader:
+        pageText = re.sub(fakultaetHeader[0], '', pageText)
+
+
 
     return pageText
 
+def removeEmptyLine(string):
+    newPageText = ''
+
+    my_string = string.split('\n', 1)[0]
+    if my_string.strip():
+        newPageText += my_string
+
+    return newPageText
 
 #convert pdf files in a dictionary into txt files
 directory = 'data/examination_regulations'
