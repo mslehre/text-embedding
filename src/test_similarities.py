1c02398fe10645455e42c67867efeee712fe24c8
s-lohenz <louisa.henze@stud.uni-greifswald.de>
2023-06-20 10:51:04 +0000

    first resolved individual comments

--- src/test_similarities.py
+++ src/test_similarities.py
@@ -3,12 +3,11 @@ import os
 from compute_embedding import compute_similarity_of_texts
 
 def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
-    """This function reads in a file line by line and splits each line into
-    a list. Each line should be split at the space character and should contain
-    five strings. The first four strings are filenames of text files which have
-    to be stored in /data/testTextPairs_WebForm. The cosine similarity of the
-    texts in the first two files and the cosine similarity of the texts in the
-    last two files is computed. The similarities of the files are written to
+    """This function reads in a tab-separated text table with 5 columns. The 
+    first four strings are filenames of text files which have to be stored in 
+    /data/testTextPairs_WebForm. The cosine similarity of the texts in the 
+    first two files and the cosine similarity of the texts in the last two 
+    files is computed. The similarities of the files are written to 
     filename_out. It is tested which cosine similarity is higher or rather the
     content of which of the files is more similar. The result is written to
     filename_out. The fifth string in each line is a boolean value which
@@ -25,8 +24,8 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
     Returns:
         None: This function does not return a value.
     """
-    # The files that the cosine similarity is computed for has to be in 
-    # the directory testTextPais_WebForm.
+    # the directory containing the files that the cosine similarity is computed 
+    # for 
     path_texts = '../data/testTextPairs_WebForm/'
     # Open the file that contains the filenames of the files that the cosine
     # similarity is computed for and read it line by line.
@@ -38,16 +37,16 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
 
     for i in range(0, len(lines)):
         # Split each line at the space character to get the filenames.
-        lines[i] = lines[i].split(" ")
+        files = lines[i].split("\t")
         texts = []  # Content of the files is stored in this list.
-        for j in range(0, len(lines[i]) - 1):
-            lines[i][j] = lines[i][j].strip()
+        for j in range(0, len(files) - 1):
+            files[j] = files[j].strip()
             # Test if a file with the filename specified in the input file 
             # exists. If not write it to the output file and continue with the 
             # next line in the input file.
-            if not (os.path.exists(path_texts + lines[i][j])):
+            if not (os.path.exists(path_texts + files[j])):
                 with open('../data/' + filename_out, "a") as file_out:
-                    file_out.write("File " + lines[i][j] + " does not exist " \
+                    file_out.write("File " + files[j] + " does not exist " \
                         "in text-embedding/data/testTextPairs_WebForm/. " \
                         "Therefore, nothing will be computed for line " + 
                         str(i + 1) + " in " + filename_in + ".\n")
@@ -55,7 +54,7 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
                 break
             else:
                 # Store the content of the file in the list texts.
-                file = open(path_texts + lines[i][j], "r")
+                file = open(path_texts + files[j], "r")
                 texts.append(file.read())
                 file.close()
         if (len(texts) == 4):
@@ -64,7 +63,7 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
             similarity_2 = compute_similarity_of_texts(texts[2], texts[3])
             # Test if the cosine similarity or rather the embeddings of the 
             # texts could be computed.
-            if (similarity_1 == -2.0 or similarity_2 == -2.0):
+            if (similarity_1 is None or similarity_2 is None):
                 with open('../data/' + filename_out, "a") as file_out:
                     file_out.write("The embedding for one of the files in " \
                         "line" + str(i + 1) + " in " + filename_in + " could " \
@@ -75,9 +74,9 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
                 # Write the cosine similarities to the output file.
                 with open('../data/' + filename_out, "a") as file_out:
                     file_out.write("The cosine similarity of the texts in " 
-                        + lines[i][0] + " and " + lines[i][1] + " is " 
+                        + files[0] + " and " + files[1] + " is " 
                         + str(similarity_1) + " and for the texts in " 
-                        + lines[i][2] + " and " + lines[i][3] + " it is "
+                        + files[2] + " and " + files[3] + " it is "
                         + str(similarity_2) + ". ")
                     file_out.close()
 
@@ -88,11 +87,11 @@ def check_cosine_similarity(filename_in: str, filename_out: str) -> None:
                 if (similarity_1 < similarity_2):
                     order_of_texts = [2, 0]  
                 with open('../data/' + filename_out, "a") as file_out:
-                    file_out.write("The texts in " + lines[i][order_of_texts[0]]
-                        + " and " + lines[i][order_of_texts[0] + 1] + " are " \
-                        "more similar than the texts in " 
-                        + lines[i][order_of_texts[1]] + " and " 
-                        + lines[i][order_of_texts[1] + 1] + ". ")
+                    file_out.write("The texts in " + files[order_of_texts[0]]
+                        + " and " + files[order_of_texts[0] + 1] + " are " \
+                        "at least as similar as the texts in " 
+                        + files[order_of_texts[1]] + " and " 
+                        + files[order_of_texts[1] + 1] + ". ")
 
                 prediction_right = ""
                 # Test if the prediction in filename_in is right and write the
