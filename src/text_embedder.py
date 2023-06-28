65e6abd5fbebf16f70ac143fae7d5b806d66168f
s-nojung <s-nojung@uni-greifswald.de>
2023-06-28 13:01:06 +0000

    seperated files for adaption to examination regulations

--- src/text_embedder.py
+++ src/text_embedder.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 import sys
 from pathlib import Path
@@ -8,27 +6,6 @@ import numpy as np
 import h5py
 from compute_embedding import embedding_from_string
 
-
-def try_to_read_dir(dir_path: str) -> str:
-    # a quick function for argparse to check if given arg is a readable dir
-    if not os.path.isdir(dir_path) or not os.access(dir_path, os.R_OK):
-        raise argparse.ArgumentTypeError("The directory " + dir_path + " does "
-                                         + "not exist or is not readable!")
-    return dir_path
-
-def try_to_read_file(file_path: str) -> str:
-    # a quick function for argparse to check if given arg is a readable file
-    if not os.path.isfile(file_path) or not os.access(file_path, os.R_OK):
-        raise argparse.ArgumentTypeError("The file " + file_path + " does not "
-                                         + "exist or is not readable!")
-    return file_path
-
-def try_to_write_file_if_exists(file_path: str) -> str:
-    # a quick function for argparse to check if given arg is a writable file
-    if os.path.isfile(file_path) and not os.access(file_path, os.W_OK):
-        raise argparse.ArgumentTypeError("The file " + file_path + " is not "
-                                         + "writable!")
-    return file_path
 
 def read_pub(file_path: str) -> str:
     """
@@ -43,6 +20,7 @@ def read_pub(file_path: str) -> str:
     pubs = []
     with open(file_path, 'r') as file_handle:
         lines = file_handle.readlines()
+        pubs.append(lines[0])
         for line in lines[1:]:  # exclude first line containing author
             if line.strip():  # check if line is not empty
                 # add publication title and ignore link
@@ -158,86 +136,3 @@ def write_hdf5(hdf5_file: str,
                     # add new author at the end of the datasets
                     ids[n] = id
                     pub_embeddings[n, :] = embedding
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Compute embeddings for lists of publications.')
-    parser.add_argument('-p', '--publications', type = try_to_read_dir,
-                        metavar = 'DIR', 
-                        help = 'Directory containing files with names ' 
-                        + '<author_id>.txt, which contain publication lists.')
-    parser.add_argument('-n', '--num_authors', type = int, metavar = 'INT',
-                        help = 'Number of expected files in the publications ' 
-                        + 'directory, i.e. if the highest author ID in the ' 
-                        + 'directory is 10, specify 11 as the indexing starts '
-                        + 'with 0. All files in {0.txt, ... n-1.txt} are ' 
-                        + 'tried, a subset may be missing.')
-    parser.add_argument('-u', '--update', nargs = '+', type = try_to_read_file,
-                        metavar='FILE',
-                        help = 'Update existing embeddings in hdf5 file with '
-                        +' the publications in the specified file(s). The '
-                        + ' names of the files must again have format '
-                        + '<author_id>.txt. If the author ID already exists in'
-                        + ' the HDF5 file, the corresponding embedding will be'
-                        + ' overwritten, if not, it will be added to the '
-                        + 'dataset. This option is mutually exclusive with '
-                        + '\'--publications\'.')
-    parser.add_argument('-f', '--hdf5_file', required = True,
-                        type = try_to_write_file_if_exists,
-                        help = 'Output file in hdf5 format with data sets: ' 
-                        + 'publication_embedding: 2-dim numpy array with '
-                        + 'embeddings. author_ids: 1-dim numpy array '
-                        + 'containing the ids for each embedding.')
-    args = parser.parse_args()
-
-    # either compute new embedding or update existing embedding
-    if args.publications == args.update:
-        print("ERROR: You need to specifiy exactly one of the 2 arguments: " 
-              + "'--publications' or '--update'.")
-        exit(1)
-    # arguments --publications and --num_authors need to be specified together
-    if args.publications and not args.num_authors:
-        print("ERROR: You need to specify argument '--num_authors' with "
-              + "argument '--publications'.")
-        exit(1)
-    # check if file to update exists
-    if args.update and not os.path.isfile(args.hdf5_file):
-        print("ERROR: File", args.hdf5_file, "does not exist! Please specify"
-            + " a HDF5 file to update.")
-
-    # compute embeddings for publications in directory
-    if args.publications:
-        # read publications and compute embeddings
-        pubs, author_ids = read_pubs_in_dir(args.publications, 
-                                            args.num_authors)
-        embeddings = embeddings_from_pubs(pubs)
-        # write data to hdf5 file
-        write_hdf5(args.hdf5_file, embeddings, author_ids)
-    
-    else:  # update existing embedding
-        author_ids = []
-        pubs = []
-        for file_path in args.update:
-            # check if file names have format <author_id>.txt
-            file_stem = Path(file_path).stem
-            try:
-                id = int(file_stem)
-            except ValueError:
-                print("ERROR: The name of file \"", file_path, "\" does not " 
-                + "have format <author_id>.txt!")
-                exit(1)
-            # read if yes
-            author_pubs = read_pub(file_path)
-            author_ids.append(id)
-            pubs.append(author_pubs)
-        # compute embeddings            
-        embeddings = embeddings_from_pubs(pubs)
-        author_ids = np.asarray(author_ids)
-        # update hdf5 file
-        write_hdf5(args.hdf5_file, embeddings, author_ids, update = True)
-
-    exit(0)
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/txt_to_hdf5.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+import argparse
+import numpy as np
+import h5py
+
+from text_embedder import read_pub, read_pubs_in_dir, embeddings_from_pubs, write_hdf5
+
+def try_to_read_dir(dir_path: str) -> str:
+    # a quick function for argparse to check if given arg is a readable dir
+    if not os.path.isdir(dir_path) or not os.access(dir_path, os.R_OK):
+        raise argparse.ArgumentTypeError("The directory " + dir_path + " does "
+                                         + "not exist or is not readable!")
+    return dir_path
+
+def try_to_read_file(file_path: str) -> str:
+    # a quick function for argparse to check if given arg is a readable file
+    if not os.path.isfile(file_path) or not os.access(file_path, os.R_OK):
+        raise argparse.ArgumentTypeError("The file " + file_path + " does not "
+                                         + "exist or is not readable!")
+    return file_path
+
+def try_to_write_file_if_exists(file_path: str) -> str:
+    # a quick function for argparse to check if given arg is a writable file
+    if os.path.isfile(file_path) and not os.access(file_path, os.W_OK):
+        raise argparse.ArgumentTypeError("The file " + file_path + " is not "
+                                         + "writable!")
+    return file_path
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compute embeddings for lists of publications.')
+    parser.add_argument('-p', '--publications', type = try_to_read_dir,
+                        metavar = 'DIR', 
+                        help = 'Directory containing files with names ' 
+                        + '<author_id>.txt, which contain publication lists.')
+    parser.add_argument('-n', '--num_authors', type = int, metavar = 'INT',
+                        help = 'Number of expected files in the publications ' 
+                        + 'directory, i.e. if the highest author ID in the ' 
+                        + 'directory is 10, specify 11 as the indexing starts '
+                        + 'with 0. All files in {0.txt, ... n-1.txt} are ' 
+                        + 'tried, a subset may be missing.')
+    parser.add_argument('-u', '--update', nargs = '+', type = try_to_read_file,
+                        metavar='FILE',
+                        help = 'Update existing embeddings in hdf5 file with '
+                        +' the publications in the specified file(s). The '
+                        + ' names of the files must again have format '
+                        + '<author_id>.txt. If the author ID already exists in'
+                        + ' the HDF5 file, the corresponding embedding will be'
+                        + ' overwritten, if not, it will be added to the '
+                        + 'dataset. This option is mutually exclusive with '
+                        + '\'--publications\'.')
+    parser.add_argument('-f', '--hdf5_file', required = True,
+                        type = try_to_write_file_if_exists,
+                        help = 'Output file in hdf5 format with data sets: ' 
+                        + 'publication_embedding: 2-dim numpy array with '
+                        + 'embeddings. author_ids: 1-dim numpy array '
+                        + 'containing the ids for each embedding.')
+    args = parser.parse_args()
+
+    # either compute new embedding or update existing embedding
+    if args.publications == args.update:
+        print("ERROR: You need to specifiy exactly one of the 2 arguments: " 
+              + "'--publications' or '--update'.")
+        exit(1)
+    # arguments --publications and --num_authors need to be specified together
+    if args.publications and not args.num_authors:
+        print("ERROR: You need to specify argument '--num_authors' with "
+              + "argument '--publications'.")
+        exit(1)
+    # check if file to update exists
+    if args.update and not os.path.isfile(args.hdf5_file):
+        print("ERROR: File", args.hdf5_file, "does not exist! Please specify"
+            + " a HDF5 file to update.")
+
+    # compute embeddings for publications in directory
+    if args.publications:
+        # read publications and compute embeddings
+        pubs, author_ids = read_pubs_in_dir(args.publications, 
+                                            args.num_authors)
+        embeddings = embeddings_from_pubs(pubs)
+        # write data to hdf5 file
+        write_hdf5(args.hdf5_file, embeddings, author_ids)
+    
+    else:  # update existing embedding
+        author_ids = []
+        pubs = []
+        for file_path in args.update:
+            # check if file names have format <author_id>.txt
+            file_stem = Path(file_path).stem
+            try:
+                id = int(file_stem)
+            except ValueError:
+                print("ERROR: The name of file \"", file_path, "\" does not " 
+                + "have format <author_id>.txt!")
+                exit(1)
+            # read if yes
+            author_pubs = read_pub(file_path)
+            author_ids.append(id)
+            pubs.append(author_pubs)
+        # compute embeddings            
+        embeddings = embeddings_from_pubs(pubs)
+        author_ids = np.asarray(author_ids)
+        # update hdf5 file
+        write_hdf5(args.hdf5_file, embeddings, author_ids, update = True)
+
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
