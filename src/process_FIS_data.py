924a11d8d33244bd8cbb88f624f7d56f2566ec87
s-lohenz <louisa.henze@stud.uni-greifswald.de>
2023-07-09 11:13:40 +0000

    check if person in publication data also exists in person data

--- src/process_FIS_data.py
+++ src/process_FIS_data.py
@@ -8,8 +8,30 @@ publication_data = pd.read_csv('../data/FIS/Publikationen.csv', sep=';',
 person_data = pd.read_csv('../data/FIS/Personen_Einrichtungen_2023_06.csv', 
                           sep=';', names = ["person_ID", "lastname", "forename",
                           "institution", "faculty"], skiprows=1, encoding='latin-1')
-publication_data['lastname'] = publication_data['person_ID'].apply(lambda x: person_data.loc[person_data['person_ID'] == x, 'lastname'].values[0])
-publication_data['forename'] = publication_data['person_ID'].apply(lambda x: person_data.loc[person_data['person_ID'] == x, 'forename'].values[0])
+
+# Create empty lists to store the lastnames and forenames
+lastnames = []
+forenames = []
+    
+# Iterate over the person_IDs in the publication_data
+for person_ID in publication_data["person_ID"]:
+    # Check if the person_ID is in the person_data
+    if person_ID in person_data["person_ID"].values:
+        # If it is in the person_data, get the lastname and forename
+        lastname = person_data.loc[person_data["person_ID"] == person_ID, "lastname"].values[0]
+        forename = person_data.loc[person_data["person_ID"] == person_ID, "forename"].values[0]
+    else:
+        # If it is not in the person_data, set the lastname and forename to "NaN"
+        lastname = "NaN"
+        forename = "NaN"
+    # Append the lastname and forename to the lists
+    lastnames.append(lastname)
+    forenames.append(forename)
+    
+# Add the lists as new columns to the publication_data dataframe
+publication_data["lastname"] = lastnames
+publication_data["forename"] = forenames
+
 # Write a fuction that finds for each person_ID in publication_data all entries 
 # in publication_data with the same person_ID and joins the titles of the
 # publications into the same row separared by a tab and adds these publications
