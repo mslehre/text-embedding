bc9a2a24aaed6342ae393ab382ea109845db1042
NoJungi <nora@jungierek.de>
2023-07-10 11:30:17 +0200

    added meta files and meta file for all texts in seperator list

--- src/ask_question.py
+++ src/ask_question.py
@@ -8,31 +8,41 @@ from build_prompt import get_prompt
 def get_answer(
         query: str, 
         text_dir: str,
-        id_list: list[str],
-        seperator_list: list[str] = None) -> str:
+        id_list: list[str]) -> str:
     """From a question asked by the user, generate the answer
 
     Args:
         query (str): Question asked by the user.
         text_dir (str): Documents directory.
         id_list (list[str]): List of relevant docs.
-        seperator_list (list[str]): List of strings to insert as seperators in
-            between the text chunks.
 
     Returns:
         str: Answer generated with the LLM
     """
 
-    # First read in the chunks given by the id_list from the given directory
+    # First read in the chunks given by the id_list from the given directory.
     # Either the file exists as it is in the directory or it is located in a 
-    # sub directory with the chubks of a larger text file.
+    # sub directory (file name without chunk id) with the chunks of a larger 
+    # text file.
     docs = []
+    seperator_list = []
+
+    # add meta data that holds for all files in the directory
+    meta_file = os.path.join(text_dir, 'meta.txt')  # meta file for all texts
+    if os.path.isfile(meta_file) and os.access(meta_file, os.R_OK):
+        with open(meta_file, 'r') as file:
+                meta_data = file.read()
+        seperator_list.append(f'Here are meta information that hold for all '
+        + f'texts that are given to answer the question:\n{meta_data}\n\n')
+
     for i in id_list:
         file_path = os.path.join(text_dir, i + ".txt")
         dir_path = os.path.join(text_dir, i.split('.')[0]) # sub dir
+        file_in_sub_dir = False
         # search for a sub directory with chunks:
         if(not os.path.isfile(file_path) and os.path.isdir(dir_path)):
             file_path = os.path.join(dir_path, i + ".txt")
+            file_in_sub_dir = True  # to get the location of the meta file
 
         if not os.access(file_path, os.R_OK):
             print(f'ERROR: Could not find or acces the file {i}.txt '
@@ -42,7 +52,29 @@ def get_answer(
         this_chunk = open(file_path, "r", encoding="UTF-8")
         docs.append(this_chunk.read())
         this_chunk.close()
+
+        # Create seperator for the chunk using the meta file data if a meta 
+        # file exist:
+        if file_in_sub_dir == True:
+            meta_file = os.path.join(dir_path, i.split('.')[0] + '.meta.txt')
+        else:
+            meta_file = os.path.join(text_dir, i.split('.')[0] + '.meta.txt')
+
+        if os.path.isfile(meta_file) and os.access(meta_file, os.R_OK):
+            # read in the meta data into seperators
+            with open(meta_file, 'r') as file:
+                meta_data = file.read()
+            seperator_list.append(f'Here are some meta information about the '
+                                  + f'following text:\n{meta_data}.\n The '
+                                  + 'corresponding text:')
+        else:
+            # ignore meta file, continue without meta information
+            seperator_list.append('This is a new text:')
         
+    if len(seperator_list) == (len(docs) +1):
+        meta_data = seperator_list.pop(0)
+        seperator_list[0] = meta_data + seperator_list[0]
+
     #assemble the prompt
     this_prompt = get_prompt(query, docs, seperator_list)
     if (this_prompt == None):
@@ -61,7 +93,7 @@ def get_answer(
     
 def test():
     testq = "What are common research interests of these scientists?"
-    testdir = "data/example_pubs"
-    testlist = [2,4]
+    testdir = "data/publications"
+    testlist = ['2','4']
     testanswer = get_answer(query=testq, text_dir=testdir, id_list=testlist)
     print(testanswer)
