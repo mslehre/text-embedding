f0b115137d446342e613fed6c0ef4db14b52c04d
NoJungi <nora@jungierek.de>
2023-07-17 15:47:43 +0200

    seperate function to get texts from ids to use for the web form

--- src/ask_question.py
+++ src/ask_question.py
@@ -18,13 +18,54 @@ def get_answer(
 
     Returns:
         str: Answer generated with the LLM
-    """
+    """         
+    docs,seperator_list = get_texts_from_ids(id_list=id_list,
+                                             text_dir=text_dir)
+        
+    if len(seperator_list) == (len(docs) +1):
+        meta_data = seperator_list.pop(0)
+        seperator_list[0] = meta_data + seperator_list[0]
+
+    #assemble the prompt
+    this_prompt = get_prompt(query, docs, seperator_list)
+    if (this_prompt == None):
+        return None
+
+    #call openai to obtain a response
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = this_prompt,
+        temperature = 0,
+        max_tokens = 500,
+    )
+
+    result = response['choices'][0]['text']
+    return result
+
+def get_texts_from_ids(id_list: list[str],
+                       text_dir: str) -> tuple[list[str]]:
+    """Returns a tuple with the texts from the text chunks with the specified 
+    IDs and a list with the information from the according meta files. If no 
+    meta file exists, the second list contains 'Here is the next text' as an
+    entry. The second list can be used as a seperator list for the prompt 
+    building.
+    
+    Args:
+        id_list (list[str]): List with the IDs of the sleected chunks.
+        text_dir (str): Documents directory.
+        
+    Returns:
+        list[str]: A list that contains the texts from the chunks with the 
+            selected IDs.
+        list[str]: A list that contains the meta information form the 
+            corresponding meta files if they exist.
+        """
+    docs = []
+    seperator_list = []
 
     # First read in the chunks given by the id_list from the given directory.
     # Either the file exists as in the directory or it is located in a sub 
     # directory, file name without chunk id.
-    docs = []
-    seperator_list = []
 
     # add meta data that holds for all files in the directory if exists
     meta_file = os.path.join(text_dir, 'meta.txt')  # meta file for all texts
@@ -34,7 +75,7 @@ def get_answer(
         seperator_list.append(f'Here is some meta information that hold for '
                               + f'all texts that are given to answer the '
                               + f'question:\n{meta_data}\n\n')
-        
+
     for id in id_list:
         file_path = os.path.join(text_dir, id + ".txt")
         dir_path = os.path.join(text_dir, id.split('.')[0]) # sub dir
@@ -70,26 +111,8 @@ def get_answer(
         else:
             # ignore meta file, continue without meta information
             seperator_list.append('This is a new text:')
-        
-    if len(seperator_list) == (len(docs) +1):
-        meta_data = seperator_list.pop(0)
-        seperator_list[0] = meta_data + seperator_list[0]
-
-    #assemble the prompt
-    this_prompt = get_prompt(query, docs, seperator_list)
-    if (this_prompt == None):
-        return None
-
-    #call openai to obtain a response
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = this_prompt,
-        temperature = 0,
-        max_tokens = 500,
-    )
-
-    result = response['choices'][0]['text']
-    return result
+    
+    return docs, seperator_list
     
 def test():
     testq = "What are common research interests of these scientists?"
